76c1c240977e0fb4985465bbc6eac112b2a0642e
Johnny Miller <163300+millerjp@users.noreply.github.com>
2025-12-09 15:31:05 +0100

    Update rebuild.sh script with required build args
    Added all required build args to quick rebuild script:
    - CASSANDRA_VERSION (hardcoded 5.0.6 for now)
    - MAJOR_VERSION (detected from directory)
    - K8SSANDRA_BASE_DIGEST (env var or default)
    - K8SSANDRA_API_VERSION (env var or default 0.1.110)
    - CQLAI_VERSION (env var or default 0.0.31)
    
    Script now works with new Dockerfile requirements.
    
    Optional args (BUILD_DATE, VCS_REF, etc.) omitted - will default
    to 'unknown' which is fine for quick dev/test builds.

--- k8ssandra/scripts/rebuild.sh
+++ k8ssandra/scripts/rebuild.sh
@@ -12,7 +12,24 @@ fi
 kubectl delete -f cluster-axonops.yaml
 
 crictl images | grep ttl.sh | awk '{print $3}' | xargs crictl rmi
-docker build -t $IMAGE_NAME . && docker push $IMAGE_NAME
+
+# Get k8ssandra version from K8SSANDRA_VERSIONS variable or use latest known
+# For quick dev builds, we use the first available version
+CASSANDRA_VER=$(basename $(pwd) | cut -d. -f1,2)  # Get 5.0 from directory
+CQLAI_VER=${CQLAI_VERSION:-0.0.31}
+
+# Note: These should match your K8SSANDRA_VERSIONS variable
+# For 5.0.6, use the digest from the variable
+K8S_DIGEST=${K8SSANDRA_DIGEST:-sha256:aa2de19866f3487abe0dff65e6b74f5a68c6c5a7d211b5b7a3e0b961603ba5af}
+K8S_API_VER=${K8SSANDRA_API_VERSION:-0.1.110}
+
+docker build \
+  --build-arg CASSANDRA_VERSION=5.0.6 \
+  --build-arg MAJOR_VERSION=$CASSANDRA_VER \
+  --build-arg K8SSANDRA_BASE_DIGEST=$K8S_DIGEST \
+  --build-arg K8SSANDRA_API_VERSION=$K8S_API_VER \
+  --build-arg CQLAI_VERSION=$CQLAI_VER \
+  -t $IMAGE_NAME . && docker push $IMAGE_NAME
 crictl pull $IMAGE_NAME
 
 cat cluster-axonops.yaml | envsubst > /tmp/cluster-axonops.yaml
