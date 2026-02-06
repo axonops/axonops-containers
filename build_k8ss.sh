c0b72a7fffff4d715c50bd34a38e50722f74c20e
Marc Magnin <marc.magnin@digitalis.io>
2026-02-06 11:52:21 +0100

    added axonagent vers as arg

--- build_k8ss.sh
+++ build_k8ss.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-AXON_AGENT_JAVA_VERSION=$1
+#./build_k8ss.sh "2.0.3.asb14061-1.aarch64" "1.0.13.asb4142nodetoolscheduler502-1"
+AXON_AGENT_VERSION=$1 
+AXON_AGENT_JAVA_VERSION=$2
 
 crictl images | grep ttl.sh | awk '{print $3}' | xargs crictl rmi
 
-docker build --build-arg=CQLAI_VERSION=0.1.1 --build-arg=AXON_AGENT_JAVA_VERSION=${AXON_AGENT_JAVA_VERSION} --build-arg CASSANDRA_VERSION=5.0.6 --build-arg MAJOR_VERSION=5.0 --build-arg K8SSANDRA_BASE_DIGEST=sha256:bc5708b8ac40c2ad027961a2b1e1b70c826468b8b727c30859718ffc24d7ae04 --build-arg K8SSANDRA_API_VERSION=0.1.111 --tag ttl.sh/marc-axonops-cassandra:1h ./k8ssandra/5.0
+docker build --build-arg=CQLAI_VERSION=0.1.1 --build-arg=AXON_AGENT_VERSION=${AXON_AGENT_VERSION} --build-arg=AXON_AGENT_JAVA_VERSION=${AXON_AGENT_JAVA_VERSION} --build-arg CASSANDRA_VERSION=5.0.6 --build-arg MAJOR_VERSION=5.0 --build-arg K8SSANDRA_BASE_DIGEST=sha256:bc5708b8ac40c2ad027961a2b1e1b70c826468b8b727c30859718ffc24d7ae04 --build-arg K8SSANDRA_API_VERSION=0.1.111 --tag ttl.sh/marc-axonops-cassandra:1h ./k8ssandra/5.0
 
 [ $? -gt 0 ] && {
   echo "failed"
